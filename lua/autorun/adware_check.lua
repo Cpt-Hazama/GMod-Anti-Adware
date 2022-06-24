b24bd26fb4175b1ceb589f93277c39a8c5e67c8b
Cpt-Hazama <Caleb123459@gmail.com>
2022-06-24 17:36:58 -0400

    Optimization

--- lua/autorun/adware_check.lua
+++ lua/autorun/adware_check.lua
@@ -2,21 +2,25 @@
 // It is not 100% accurate, but it is good enough for most purposes.
 // To view all found adware, open your console and paste this into it: lua_run listViruses()
 
+local table_value = table.HasValue
+local table_insert = table.insert
+local string_find = string.find
+local httpFetch = http.Fetch
+
 local safe = {
     [".com"] = true,
     [".org"] = true,
 }
 local foundViruses = {}
 
-local httpFetch = http.Fetch
 http.Fetch = function(url,...)
     for domain,__ in pairs(safe) do
-        if !string.find(url,domain) then
-			if !table.HasValue(foundViruses,url) then
+        if !string_find(url,domain) then
+			if !table_value(foundViruses,url) then
 				if IsValid(Entity(1)) then
 					Entity(1):ChatPrint("Adware Detected! Check your console for details.")
 				end
-				table.insert(foundViruses,url)
+				table_insert(foundViruses,url)
 			end
 			print("ADWARE DETECTED! BLOCKING THE FOLLOWING URL: " .. url)
 			return
